b67e1f211dec3ff2b8110689bf692f217c09fe5f
Jenny Chau <jnechau@gmail.com>
2022-01-11 19:37:05 -0500

    commit tree bf5cd01a017d344584f31f1c8b0844066bed8f64

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :sufficiently_clickbaity?
+
+    def sufficiently_clickbaity?
+        if [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i].none? {|pattern| pattern.match title}
+            errors.add(:title, "error: must be clickbait")
+        end
+    end
 end
